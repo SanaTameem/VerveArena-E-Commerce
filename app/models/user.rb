c0ad98b1d7695835fcafde3f1ee598c80ab0c54b
SanaTameem <71990110+SanaTameem@users.noreply.github.com>
2024-03-14 23:05:21 +0430

    fix linter issues

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :jwt_authenticatable, jwt_revocation_strategy: self
 
-  def jwt_payload
-    super
-  end
+  # def jwt_payload
+  #   super
+  # end
 end
