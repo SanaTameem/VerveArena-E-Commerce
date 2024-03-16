05020de68d95f9bae505de686bd155e8d2f83022
SanaTameem <71990110+SanaTameem@users.noreply.github.com>
2024-03-16 20:52:06 +0430

    fix linter issues

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -6,5 +6,4 @@ class OrderItem < ApplicationRecord
   validates :product_id, presence: true
   validates :quantity, presence: true, numericality: { greater_than: 0 }
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
-
 end
