713d5432c7cd6fdf366a4aa501902563880ddbf2
SanaTameem <71990110+SanaTameem@users.noreply.github.com>
2024-03-16 20:45:22 +0430

    Update  app/models/payment.rb file

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,3 +1,7 @@
 class Payment < ApplicationRecord
   belongs_to :order
+
+  validates :paid_amount, presence: true, numericality: { greater_than: 0 }
+  validates :payment_date, presence: true
+  validates :order_id, presence: true
 end
