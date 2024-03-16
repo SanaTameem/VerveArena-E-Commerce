6f0f1e618b5eb449dd927581e1bc2776817ced55
SanaTameem <71990110+SanaTameem@users.noreply.github.com>
2024-03-16 20:44:25 +0430

    Update app/models/cart_item.rb file

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,4 +1,8 @@
 class CartItem < ApplicationRecord
   belongs_to :cart
   belongs_to :product
+
+  validates :cart_id, presence: true
+  validates :product_id, presence: true
+  validates :quantity, presence: true, numericality: { only_integer: true, greater_than: 0 }
 end
