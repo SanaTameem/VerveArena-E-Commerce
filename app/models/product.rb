ff9a97664f2e724a4bb403bf9e19b61611270866
SanaTameem <71990110+SanaTameem@users.noreply.github.com>
2024-03-16 20:45:35 +0430

    Update app/models/product.rb file

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,4 +6,13 @@ class Product < ApplicationRecord
   serialize :size, Array, coder: JSON
   serialize :color, Array, coder: JSON
   serialize :image_url, Array, coder: JSON
+
+  validates :sub_category_id, presence: true
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :stock_quantity, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :image_url, presence: true
+  validates :gender_target, inclusion: { in: %w(men women kids) }
+  validates :rating, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 5 }, allow_nil: true
 end
