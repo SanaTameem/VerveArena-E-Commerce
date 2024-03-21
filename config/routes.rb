fe182e4c2ff385f700d7f9b67c2e9466b20d0fe7
SanaTameem <71990110+SanaTameem@users.noreply.github.com>
2024-03-22 00:39:19 +0430

    Update config/routes.rb file

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      # resources :users do
-        resources :categories, only: [:index, :show, :create, :update, :destroy] do
-          resources :sub_categories, only: [:index, :show, :create, :update, :destroy]
+      resources :users do
+        resources :carts, only: [:show, :create] do
+          resources :cart_items, only: [:index, :create, :update, :destroy]
         end
-      # end
+        resources :categories, only: [:index, :show, :create, :update, :destroy] do
+          resources :sub_categories, only: [:index, :show, :create, :update, :destroy] do
+            resources :products, only: [:index, :show, :create, :update, :destroy]
+          end
+        end
+      end
     end
   end
   devise_for :users, path: 'auth', path_names: {
