86e915c8a2d2e5c6e0bf11583c9346091c009612
SanaTameem <71990110+SanaTameem@users.noreply.github.com>
2024-03-22 00:39:03 +0430

    Update app/models/product.rb file

--- app/models/product.rb
+++ app/models/product.rb
@@ -13,7 +13,7 @@ class Product < ApplicationRecord
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :stock_quantity, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :image_url, presence: true
-  validates :gender_target, inclusion: { in: %w[men women kids] }
-  validates :rating, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 5 },
+  validates :gender_target, inclusion: { in: %w[men women kids] }, allow_nil: true
+  validates :rating, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5 },
                      allow_nil: true
 end
