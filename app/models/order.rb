f35270b2033b7337d15ddd736873d12b2ab028dd
SanaTameem <71990110+SanaTameem@users.noreply.github.com>
2024-03-23 00:24:14 +0430

    Update app/models/order.rb file

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,10 +1,10 @@
 class Order < ApplicationRecord
   belongs_to :user
-  has_many :order_items
-  has_one :payment
+  # has_many :order_items
+  has_one :payment, dependent: :destroy
 
   validates :user_id, presence: true
   validates :order_date, presence: true
-  validates :status, presence: true
+  validates :status, presence: true, inclusion: { in: %w[pending shipped delivered cancelled] }
   validates :total_amount, presence: true, numericality: { greater_than_or_equal_to: 0 }
 end
