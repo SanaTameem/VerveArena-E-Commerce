43430d1ad5c7d8846f4af7365e0ea162eb88e9bb
SanaTameem <71990110+SanaTameem@users.noreply.github.com>
2024-03-24 23:40:50 +0430

    Update app/models/user.rb file

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,20 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :users do
-        resources :orders, only: [:index, :show, :create, :update, :destroy] do
-          resources :payments, only: [:show, :create, :destroy]
-          # resources :order_items, only: [:index, :create, :update, :destroy]
-        end
-        resources :carts, only: [:show, :create] do
-          resources :cart_items, only: [:index, :create, :update, :destroy]
-        end
-        resources :categories, only: [:index, :show, :create, :update, :destroy] do
-          resources :sub_categories, only: [:index, :show, :create, :update, :destroy] do
-            resources :products, only: [:index, :show, :create, :update, :destroy]
-          end
+      # resources :users do
+      resources :orders, only: [:index, :show, :create, :update, :destroy] do
+        resources :payments, only: [:show, :create, :destroy]
+        # resources :order_items, only: [:index, :create, :update, :destroy]
+      end
+      resources :carts, only: [:show, :create] do
+        resources :cart_items, only: [:index, :create, :update, :destroy]
+      end
+      resources :categories, only: [:index, :show, :create, :update, :destroy] do
+        resources :sub_categories, only: [:index, :show, :create, :update, :destroy] do
+          resources :products, only: [:index, :show, :create, :update, :destroy]
         end
       end
+      # end
     end
   end
   devise_for :users, path: 'auth', path_names: {
