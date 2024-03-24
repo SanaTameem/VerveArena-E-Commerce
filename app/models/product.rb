4599b20ecfdd4e99bd1f525688f2417c1049294d
SanaTameem <71990110+SanaTameem@users.noreply.github.com>
2024-03-24 23:39:45 +0430

    Update app/models/product.rb file

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,7 @@
 class Product < ApplicationRecord
   belongs_to :sub_category
-  has_many :cart_items
-  has_many :order_items
+  has_many :cart_items, dependent: :destroy
+  # has_many :order_items, dependent: :destroy
 
   serialize :size, Array, coder: JSON
   serialize :color, Array, coder: JSON
@@ -13,7 +13,7 @@ class Product < ApplicationRecord
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :stock_quantity, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :image_url, presence: true
-  validates :gender_target, inclusion: { in: %w[men women kids] }, allow_nil: true
+  validates :gender_target, inclusion: { in: %w[men women kids unisex] }, allow_nil: true
   validates :rating, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5 },
                      allow_nil: true
 end
