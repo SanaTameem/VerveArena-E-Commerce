bf87d231043acbddc0651b56744bb22f4bc9e10a
SanaTameem <71990110+SanaTameem@users.noreply.github.com>
2024-03-24 23:37:14 +0430

    Update app/controllers/api/v1/carts_controller.rb file

--- app/controllers/api/v1/carts_controller.rb
+++ app/controllers/api/v1/carts_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::CartsController < ApplicationController
   def show
-    @cart = Cart.find(params[:id])
+    @cart = current_user.cart
     if @cart
       render json: @cart, status: :ok
     else
@@ -8,14 +8,15 @@ class Api::V1::CartsController < ApplicationController
     end
   end
 
-  def create
-    @cart = Cart.new(params.require(:cart).permit(:user_id))
-    if @cart.save
-      render json: @cart, status: :created
-    else
-      render json: { errors: @cart.errors.full_messages }, status: :unprocessable_entity
-    end
-  end
+  # def create
+  #   @cart = Cart.new()
+  #   @cart.user_id = current_user.id
+  #   if @cart.save
+  #     render json: @cart, status: :created
+  #   else
+  #     render json: { errors: @cart.errors.full_messages }, status: :unprocessable_entity
+  #   end
+  # end
 
   # def update
   #   if @cart.update()
