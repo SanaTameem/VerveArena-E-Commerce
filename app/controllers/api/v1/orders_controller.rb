8bef86ab6426a578ed3dc9db16a4fef69940cc5c
SanaTameem <71990110+SanaTameem@users.noreply.github.com>
2024-03-27 14:38:51 +0430

    Update app/controllers/api/v1/orders_controller.rb file

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -30,7 +30,7 @@ class Api::V1::OrdersController < ApplicationController
   end
 
   def update
-    if @order.update(order_params)
+    if @order.update(status: params[:order][:status])
       render json: @order, status: :ok
     else
       render json: { errors: @order.errors.full_messages }, status: :unprocessable_entity
@@ -55,9 +55,9 @@ class Api::V1::OrdersController < ApplicationController
     @order = Order.find(params[:id])
   end
 
-  def order_params
-    params.require(:order).permit(:status)
-  end
+  # def order_params
+  #   params.require(:order).permit(:status)
+  # end
 
   def calculate_total_amount(items)
     total_amount = 0
