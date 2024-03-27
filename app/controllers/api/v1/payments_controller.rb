34aed0006b05cdb2989b97bcca59c58b78607c94
SanaTameem <71990110+SanaTameem@users.noreply.github.com>
2024-03-27 14:39:07 +0430

    Update app/controllers/api/v1/payments_controller.rb file

--- app/controllers/api/v1/payments_controller.rb
+++ app/controllers/api/v1/payments_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::PaymentsController < ApplicationController
-  before_action :set_order, only: %i[show create destroy]
-  def show
+  before_action :set_order, only: %i[index create destroy]
+  def index
     @payment = @order.payment
     if @payment
       render json: @payment, status: :ok
@@ -10,17 +10,17 @@ class Api::V1::PaymentsController < ApplicationController
   end
 
   def create
-    if @order.payment.present?
-      render json: { error: 'Payment has already been made for this order' }, status: :unprocessable_entity
+    # if @order.payment.present?
+    #   render json: { error: 'Payment has already been made for this order' }, status: :unprocessable_entity
+    # else
+    @payment = Payment.new(paid_amount: @order.total_amount, payment_date: Date.today)
+    @payment.order = @order
+    if @payment.save
+      render json: @payment, status: :created
     else
-      @payment = Payment.new(paid_amount: @order.total_amount, payment_date: Date.today)
-      @payment.order = @order
-      if @payment.save
-        render json: @payment, status: :created
-      else
-        render json: { errors: @payment.errors.full_messages }, status: :unprocessable_entity
-      end
+      render json: { errors: @payment.errors.full_messages }, status: :unprocessable_entity
     end
+    # end
   end
 
   def destroy
