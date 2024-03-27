b2f82349b8eab02a4f75cfd68aceaf52abe7246a
SanaTameem <71990110+SanaTameem@users.noreply.github.com>
2024-03-27 14:38:20 +0430

    Update app/controllers/api/v1/cart_items_controller.rb file

--- app/controllers/api/v1/cart_items_controller.rb
+++ app/controllers/api/v1/cart_items_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::CartItemsController < ApplicationController
   before_action :set_cart, only: %i[index create]
   before_action :set_cart_item, only: %i[update destroy]
+  before_action :cart_item_params, only: %i[create update]
 
   def index
     render json: @cart.cart_items
@@ -10,10 +11,11 @@ class Api::V1::CartItemsController < ApplicationController
     @product = Product.find(params[:cart_item][:product_id])
     @cart_item = @cart.cart_items.find_by(product_id: @product.id)
     if @cart_item
-      @cart_item.update(quantity: @cart_item.quantity + 1)
+      new_quantity = @cart_item.quantity + 1
+      @cart_item.update(cart_item_params.merge(quantity: new_quantity))
       render json: @cart_item, status: :ok
     else
-      @cart_item = @cart.cart_items.build(product_id: @product.id, quantity: 1, price: @product.price)
+      @cart_item = @cart.cart_items.build(cart_item_params.merge(quantity: 1, price: @product.price))
       if @cart_item.save
         render json: @cart_item, status: :created
       else
@@ -23,7 +25,9 @@ class Api::V1::CartItemsController < ApplicationController
   end
 
   def update
-    if @cart_item.update(params.require(:cart_item).permit(:quantity))
+    quantity = params[:cart_item][:quantity].to_i
+    @product = Product.find(params[:cart_item][:product_id])
+    if @cart_item.update(cart_item_params.merge(quantity:, price: @product.price))
       render json: @cart_item, status: :ok
     else
       render json: { errors: @cart_item.errors.full_messages }, status: :unprocessable_entity
@@ -46,5 +50,9 @@ class Api::V1::CartItemsController < ApplicationController
 
   def set_cart_item
     @cart_item = CartItem.find(params[:id])
+  end
+
+  def cart_item_params
+    params.require(:cart_item).permit(:product_id)
   end
 end
